c1862da485582e4bbe20b29d2afaf9e3e4a13e03
roycechan <roycechan89@gmail.com>
2020-03-17 01:17:20 +0800

    telegram test code

--- telegram_test.py
+++ telegram_test.py
@@ -20,6 +20,8 @@ from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
                           ConversationHandler)
 
+global search_url
+
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
